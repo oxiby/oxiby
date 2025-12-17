0f425afa74ef5d6d9888eb38446b32d77d566fe3
Jimmy Cuadra <jimmy@jimmycuadra.com>
2025-12-16 22:16:41 -0800

    Add stricter requirements for binary operations.

--- src/expr/expr_binary.rs
+++ src/expr/expr_binary.rs
@@ -27,15 +27,54 @@ impl Infer for ExprBinary {
         let lhs_type = self.lhs.infer(checker)?;
         let rhs_type = self.rhs.infer(checker)?;
 
-        // For now, assume that lhs implements the operator and that rhs is the appropriate type.
         Ok(match &self.op {
             Operator::Assign => rhs_type,
             Operator::AddAssign
             | Operator::DivAssign
             | Operator::MultAssign
-            | Operator::SubAssign => lhs_type,
-            Operator::And
-            | Operator::Or
+            | Operator::SubAssign => {
+                if lhs_type != rhs_type {
+                    return Err(Error::type_mismatch()
+                        .with_detail(
+                            &format!(
+                                "Expected right-hand side of `{}` operation to be `{lhs_type}`, \
+                                 but was `{rhs_type}`.",
+                                self.op,
+                            ),
+                            self.rhs.span(),
+                        )
+                        .finish());
+                }
+
+                lhs_type
+            }
+            Operator::And | Operator::Or => {
+                if !lhs_type.is_boolean() {
+                    return Err(Error::type_mismatch()
+                        .with_detail(
+                            &format!(
+                                "Expected left-hand side of `{}` operation to be `Boolean`, but \
+                                 was `{lhs_type}`.",
+                                self.op,
+                            ),
+                            self.lhs.span(),
+                        )
+                        .finish());
+                } else if !rhs_type.is_boolean() {
+                    return Err(Error::type_mismatch()
+                        .with_detail(
+                            &format!(
+                                "Expected right-hand side of `{}` operation to be `Boolean`, but \
+                                 was `{rhs_type}`.",
+                                self.op,
+                            ),
+                            self.rhs.span(),
+                        )
+                        .finish());
+                }
+
+                lhs_type
+            }
             | Operator::Eq
             | Operator::Ne
             | Operator::LtEq
@@ -43,7 +82,30 @@ impl Infer for ExprBinary {
             | Operator::Lt
             | Operator::Gt => check::Type::boolean(),
             Operator::Mul | Operator::Div | Operator::Mod | Operator::Add | Operator::Sub => {
-                check::Type::integer()
+                if !(lhs_type.is_integer() || lhs_type.is_float()) {
+                    return Err(Error::type_mismatch()
+                        .with_detail(
+                            &format!(
+                                "Expected left-hand side of `{}` operation to be `Integer` or \
+                                 `Float`, but was `{lhs_type}`.",
+                                self.op
+                            ),
+                            self.lhs.span(),
+                        )
+                        .finish());
+                } else if !(rhs_type.is_integer() || rhs_type.is_float()) {
+                    return Err(Error::type_mismatch()
+                        .with_detail(
+                            &format!(
+                                "Expected right-hand side of `+` operation to be `Integer` or \
+                                 `Float`, but was `{rhs_type}`."
+                            ),
+                            self.rhs.span(),
+                        )
+                        .finish());
+                }
+
+                rhs_type
             }
             Operator::Not => unreachable!("Cannot have a binary `Not` expression."),
         })
